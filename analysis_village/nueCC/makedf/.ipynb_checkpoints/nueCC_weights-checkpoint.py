4443ad44c241292f91e963bf9f8a0c5ddda9f2ed
Castaly Fan <castaly.fan@gmail.com>
2026-05-08 16:13:54 +0000

    updated memory

--- analysis_village/nueCC/makedf/.ipynb_checkpoints/nueCC_weights-checkpoint.py
+++ analysis_village/nueCC/makedf/.ipynb_checkpoints/nueCC_weights-checkpoint.py
@@ -1,8 +1,16 @@
-# nueCC_mc_weights.py — single config, weights included
+# # nueCC_mc_weights.py — single config, weights included
+# import sys, os
+# sys.path.insert(0, os.path.dirname(os.path.abspath(args.config)))
+# from make_nueCC_df import make_nuecc_evtdf, make_nuecc_statsdf, make_nuecc_wgtdf
+# from makedf.makedf import make_hdrdf, make_potdf_bnb
+
+# DFS   = [make_nuecc_evtdf, make_nuecc_statsdf, make_hdrdf, make_potdf_bnb, make_nuecc_wgtdf]
+# NAMES = ["evt", "stats", "hdr", "pot", "mcnu"]
+
+# nueCC_weights.py — weights only
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.abspath(args.config)))
-from make_nueCC_df import make_nuecc_evtdf, make_nuecc_statsdf, make_nuecc_wgtdf
-from makedf.makedf import make_hdrdf, make_potdf_bnb
+from make_nueCC_df import make_nuecc_wgtdf
 
-DFS   = [make_nuecc_evtdf, make_nuecc_statsdf, make_hdrdf, make_potdf_bnb, make_nuecc_wgtdf]
-NAMES = ["evt", "stats", "hdr", "pot", "mcnu"]
+DFS   = [make_nuecc_wgtdf]
+NAMES = ["mcnu"]
--- run_df_maker.py
+++ run_df_maker.py
@@ -195,7 +195,7 @@ def run_grid(inputfiles):
     -N %d \\
     --disk 100GB \\
     --cpu 1 \\
-    --memory 20GB \\
+    --memory 25GB \\
     --expected-lifetime 12h \\
     "file://$(pwd)/grid_executable.sh" \\
     "%s" \\
